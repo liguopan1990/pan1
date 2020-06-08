e7da7910aee7f78a7ddfaae95538a548aacaf82f
liguopan1990 <35193368+liguopan1990@users.noreply.github.com>
2020-06-08 15:19:45 +0800

    Create image.py

---
+++ image.py
@@ -0,0 +1,29 @@
+import re
+import requests
+
+
+def dowmloadPic(html, keyword):
+    pic_url = re.findall('"objURL":"(.*?)",', html, re.S)
+    i = 1
+    print('find keyword:' + keyword)
+    for each in pic_url:
+        print('downloading' + str(i) + str(each))
+        try:
+            pic = requests.get(each, timeout=10)
+        except requests.exceptions.ConnectionError:
+            print('error')
+            continue
+
+        dir = '../images/' + keyword + '_' + str(i) + '.jpg'
+        fp = open(dir, 'wb')
+        fp.write(pic.content)
+        fp.close()
+        i += 1
+
+
+if __name__ == '__main__':
+    word = input("Input key word: ")
+    url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + word + '&ct=201326592&v=flip'
+    result = requests.get(url)
+    print(result.text)
+    dowmloadPic(result.text, word)
