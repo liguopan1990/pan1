87aba07a27a1c7e4d0487d0faece7882a48d26ea
liguopan1990 <35193368+liguopan1990@users.noreply.github.com>
2020-06-09 10:03:21 +0800

    Update image.py

--- image.py
+++ image.py
@@ -5,26 +5,28 @@ import os
 def dowmloadPic(html, keyword):
     pic_url = re.findall('"objURL":"(.*?)",', html, re.S)
     i = 1
+	
     print('find keyword:' + keyword)
-	path = r'root/app/images'
+    path = r'/root/app/images'
     os.chdir(path)
-    for each in pic_url:
-        print('downloading' + str(i) + str(each))
-        try:
-            pic = requests.get(each, timeout=10)
-        except requests.exceptions.ConnectionError:
-            print('error')
-            continue
-		
-        dir = str(keyword) + '_' + str(i) + '.jpg'
-        fp = open(dir, 'wb')
-        fp.write(pic.content)
-        fp.close()
-        i += 1
+	if i<100:
+        for each in pic_url:
+            print('downloading' + str(i) + str(each))
+            try:
+                pic = requests.get(each, timeout=10)
+            except requests.exceptions.ConnectionError:
+                print('error')
+                continue
+
+            dir = '../images/' + keyword + '_' + str(i) + '.jpg'
+            fp = open(dir, 'wb')
+            fp.write(pic.content)
+            fp.close()
+            i += 1
 
 
 if __name__ == '__main__':
-    word = input("Input key word: ")
+    word = raw_input("Input key word: ")
     url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + str(word) + '&ct=201326592&v=flip'
     result = requests.get(url)
     print(result.text)
